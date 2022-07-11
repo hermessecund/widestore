cda14453cb199b44d9322aab150c828b6f4b8b9b
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-11 19:11:32 +0000

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -7,7 +7,7 @@ import Header from './Header'
 
 const Main = () => {
   const styles = {
-    container: `h-full w-full flex flex-col mt-[50px] pr-[50px] bg-[#000] text-white overflow-hidden`,
+    container: `h-full w-full flex flex-col mt-[0px] pr-[0px] bg-[#000] text-white overflow-hidden`,
     recentTitle: `text-2xl font-bold text-center mb-[20px] text-center mt-[40px]`,
     recentTransactionsList: `flex flex-col`,
     transactionCard: `flex justify-between mb-[20px] p-[30px] bg-[#ffff33] text-white rounded-xl shadow-xl font-bold gap-[20px] text-xl`,
