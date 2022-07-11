7bb200a2fe85f626d1a42fb9379a609f342bb3f4
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-11 22:31:11 +0000

    Update Featured.js

--- components/Featured.js
+++ components/Featured.js
@@ -12,11 +12,11 @@ const Featured = () => {
     container: `h-[400px] w-full flex bg-[#000] text-white p-[0px] flex-col`,
     title: `text-2xl font-bolder mb-[20px] mt-[10px] mb-24 ml-[10px]`,
     cards: `h-full w-full flex gap-[100px] justify-between mb-[10px] ml-[10px]`,
-    card1: `h-[130px] w-[400px] p-[20px] rounded-3xl bg-gradient-to-l from-[#0d141c] to-[#42667e] relative cursor-pointer transition-all duration-300  hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
-    card2: `h-[130px] w-[400px] p-[20px] rounded-3xl bg-gradient-to-l from-[#0d141c] to-[#42667e] relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
-    card3: `h-[130px] w-[400px] p-[20px] rounded-3xl bg-gradient-to-l from-[#0d141c] to-[#42667e] relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
-    card4: `h-[130px] w-[400px] p-[20px] rounded-3xl bg-gradient-to-l from-[#0d141c] to-[#42667e] relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
-    cardCross: `h-[180px] w-[125px] rounded-3xl absolute bottom-[20px] left-[20px] transition-all duration-300 hover:scale-105 flex  overflow-hidden`,
+    card1: `h-[30px] w-[40px] p-[10px] rounded-l bg-gradient-to-l from-[#ffff33] to-[#33ffff] relative cursor-pointer transition-all duration-300  hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
+    card2: `h-[40px] w-[40px] p-[20px] rounded-1xl bg-gradient-to-l from-[#ff33ff] to-[#ffff33] relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
+    card3: `h-[50px] w-[40px] p-[20px] rounded-2xl bg-gradient-to-l from-[#ffff33] to-[#3f3f3f] relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
+    card4: `h-[60px] w-[40px] p-[20px] rounded-l bg-gradient-to-l from-[#0d141c] to-[#42667e] relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
+    cardCross: `h-[180px] w-[125px] rounded-l absolute bottom-[10px] left-[10px] transition-all duration-300 hover:scale-120 flex  overflow-hidden`,
   }
   return (
     <div className={styles.container}>
@@ -25,28 +25,28 @@ const Featured = () => {
         <div className={styles.card1}>
           <div className={styles.cardCross}>
             <video autoPlay loop muted controls='' className='object-cover'>
-              <source src='https://openseauserdata.com/files/3565db33a856b19f48396062e59e6d62.mp4#t=0.001' />
+              <source src='https://360hs.netlify.app/TC_True.gif#t=0.001' />
             </video>
           </div>
         </div>
         <div className={styles.card2}>
           <div className={styles.cardCross}>
             <video autoPlay loop muted controls='' className='object-cover'>
-              <source src='https://lh3.googleusercontent.com/iDySn3M8iR_b9Y8XlCfahHsxbO9G4SpjZAPNaltT8Xqf1SmlLSNXOPw7dcGVa_1gZtZQYtjq4u1h5t9qClKiUYPj_yVH0SoxqYGs=w600#t=0.001' />
+              <source src='https://360hs.netlify.app/HS.gif#t=0.001' />
             </video>
           </div>
         </div>
         <div className={styles.card3}>
           <div className={styles.cardCross}>
             <video autoPlay loop muted controls='' className='object-cover'>
-              <source src='https://openseauserdata.com/files/894fd3d49c7c258d202a22bb710a3416.mp4#t=0.001' />
+              <source src='https://360hs.netlify.app/TC_Day.gif#t=0.001' />
             </video>
           </div>
         </div>
         <div className={styles.card4}>
           <div className={styles.cardCross}>
             <video autoPlay loop muted controls='' className='object-cover'>
-              <source src='https://openseauserdata.com/files/022c0aad904ddbd8884b12468aaaad28.mp4#t=0.001' />
+              <source src='https://360hs.netlify.app/TC_HS.gif#t=0.001' />
             </video>
           </div>
         </div>
