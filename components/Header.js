34588c6362c9e5c99ffe2569f9d0838ec42a687a
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-11 16:38:06 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -16,7 +16,7 @@ import BuyModal from './BuyModal'
 
 const Header = () => {
   const styles = {
-    container: `h-[60px] w-full flex items-center gap-5 px-16`,
+    container: `h-[20px] w-full flex items-center gap-5 px-16`,
     logo: `flex items-center ml-[20px] cursor-pointer flex-1`,
     search: `p-[25px] mr-[30px] w-[400px] h-[40px] bg-white rounded-full shadow-lg flex flex items-center border border-black`,
     searchInput: `bg-transparent focus:outline-none border-none flex-1 items-center flex`,
@@ -33,23 +33,15 @@ const Header = () => {
         <div className={styles.logo}>
           <Image
             src={logo}
-            alt='amazon'
-            height={100}
+            alt='hermessecund'
+            height={150}
             width={150}
             className='object-cover'
           />
         </div>
-        <div className={styles.search}>
-          <input
-            type='text'
-            placeholder='Search Your Assets...'
-            className={styles.searchInput}
-          />
-          <IoMdSearch fontSize={20} />
+        <IoMdSearch fontSize={20} />
         </div>
         <div className={styles.menu}>
-          <div className={styles.menuItem}>New Releases</div>
-          <div className={styles.menuItem}>Featured</div>
           {balance ? (
             <div
               className={(styles.balance, styles.menuItem)}
