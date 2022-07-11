164961d484c4b46b697511c951dabdd77436d170
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-11 23:16:19 +0000

    Update Featured.js

--- components/Featured.js
+++ components/Featured.js
@@ -11,7 +11,7 @@ const Featured = () => {
   const styles = {
     container: `h-[400px] w-full flex bg-[#000] text-white p-[0px] flex-col`,
     title: `text-2xl font-bolder mb-[20px] mt-[10px] mb-24 ml-[10px]`,
-    cards: `h-full w-full flex gap-[100px] justify-between mb-[10px] ml-[10px]`,
+    cards: `h-full w-full flex gap-[30px] justify-between mb-[10px] ml-[10px]`,
     card1: `h-[40px] w-[40px] p-[10px] rounded-10x1 bg-gradient-to-l from-[#ffff33] to-[#33ffff] relative cursor-pointer transition-all duration-300  hover:scale-200 hover:shadow-xl border-1 border-[#33ffff]`,
     card2: `h-[40px] w-[40px] p-[20px] rounded-5xl bg-gradient-to-l from-[#ff33ff] to-[#ffff33] relative cursor-pointer transition-all duration-300 hover:scale-200 hover:shadow-xl border-1 border-[#33ffff]`,
     card3: `h-[40px] w-[40px] p-[20px] rounded-20xl bg-gradient-to-l from-[#ffff33] to-[#3f3f3f] relative cursor-pointer transition-all duration-300 hover:scale-200 hover:shadow-xl border-1 border-[#33ffff]`,
@@ -24,15 +24,15 @@ const Featured = () => {
       <div className={styles.cards}>
         <div className={styles.card1}>
           <div className={styles.cardCross}>
-            <video autoPlay loop muted controls='' className='object-cover'>
-              <source src='../assets/tcards/1_True.gif#t=0.001' />
-            </video>
+            <image className='object-cover'>
+              <source src='../assets/tcards/1_True.gif' />
+            </image>
           </div>
         </div>
         <div className={styles.card2}>
           <div className={styles.cardCross}>
             <video autoPlay loop muted controls='' className='object-cover'>
-              <source src='../assets/tcards/2_Twatch.gif#t=0.001' />
+              <source src='../assets/tcards/2_Twatch.gif' />
             </video>
           </div>
         </div>
