ad56626bbae2ad39e3956bebe43815d63582a1b9
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-11 17:45:53 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ import BuyModal from './BuyModal'
 const Header = () => {
   const styles = {
     container: `h-[10px] w-full flex items-center gap-5 px-16`,
-    logo: `flex items-center ml-[30%] cursor-pointer flex-1`,
+    logo: `flex items-center ml-[10px] cursor-pointer flex-1`,
     search: `p-[25px] mr-[30px] w-[400px] h-[40px] bg-white rounded-full shadow-lg flex flex items-center border border-black`,
     searchInput: `bg-transparent focus:outline-none border-none flex-1 items-center flex`,
     menu: `flex items-center gap-6`,
@@ -32,7 +32,7 @@ const Header = () => {
       <div className={styles.container}>
         <div className={styles.logo}>
           <Image
-            src={../assets/banner.png}
+            src={../assets/banner.gif}
             alt='hermessecund'
             height={216px}
             width={841px}
