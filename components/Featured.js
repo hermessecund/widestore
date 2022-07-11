7266c4e3756bcf373a13473a4f5b2f483c3be7a3
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-11 19:27:34 +0000

    Update Featured.js

--- components/Featured.js
+++ components/Featured.js
@@ -9,7 +9,7 @@ const Featured = () => {
   ])
 
   const styles = {
-    container: `h-[400px] w-full flex p-[20px] flex-col`,
+    container: `h-[400px] w-full flex p-[0px] flex-col`,
     title: `text-2xl font-bolder mb-[20px] mt-[30px] mb-24 ml-[40px]`,
     cards: `h-full w-full flex gap-[100px] justify-between mb-[30px] ml-[30px]`,
     card1: `h-[130px] w-[400px] p-[20px] rounded-3xl bg-gradient-to-l from-[#0d141c] to-[#42667e] relative cursor-pointer transition-all duration-300  hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
@@ -32,7 +32,7 @@ const Featured = () => {
         <div className={styles.card2}>
           <div className={styles.cardCross}>
             <video autoPlay loop muted controls='' className='object-cover'>
-              <source src='https://openseauserdata.com/files/89cba6f1544810aea19d78e664981d63.mp4#t=0.001' />
+              <source src='https://lh3.googleusercontent.com/iDySn3M8iR_b9Y8XlCfahHsxbO9G4SpjZAPNaltT8Xqf1SmlLSNXOPw7dcGVa_1gZtZQYtjq4u1h5t9qClKiUYPj_yVH0SoxqYGs=w600#t=0.001' />
             </video>
           </div>
         </div>
