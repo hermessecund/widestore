dd84ffb9ad1209702cd3fa3e51b71011f9b866ee
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-12 03:37:49 +0000

    Update Featured.js

--- components/Featured.js
+++ components/Featured.js
@@ -12,10 +12,11 @@ const Featured = () => {
     container: `h-[400px] w-[85%] flex p-[20px] flex-col`,
     title: `text-2xl font-bolder mb-[20px] mt-[30px] mb-24 ml-[40px]`,
     cards: `h-full w-full flex gap-[100px] justify-between mb-[30px] ml-[30px]`,
-    card1: `h-[130px] w-[400px] p-[20px] rounded-3xl bg-gradient-to-l from-[#0d141c] to-[#42667e] relative cursor-pointer transition-all duration-300  hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
-    card2: `h-[130px] w-[400px] p-[20px] rounded-3xl bg-gradient-to-l from-[#0d141c] to-[#42667e] relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
-    card3: `h-[130px] w-[400px] p-[20px] rounded-3xl bg-gradient-to-l from-[#0d141c] to-[#42667e] relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
-    card4: `h-[130px] w-[400px] p-[20px] rounded-3xl bg-gradient-to-l from-[#0d141c] to-[#42667e] relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-xl border-2 border-[#fb9701]`,
+    card1: `h-[130px] w-[400px] p-[10px] rounded-2xl bg-gradient-to-l from-[#ff33ff] to-[#ffff33] relative cursor-pointer transition-all duration-300 hover:scale-155 hover:shadow-xl border-2 border-[#33ffff]`,
+    card2: `h-[130px] w-[400px] p-[10px] rounded-2xl bg-gradient-to-l from-[#ff33ff] to-[#ffff33] relative cursor-pointer transition-all duration-300 hover:scale-155 hover:shadow-xl border-2 border-[#33ffff]`,
+    card3: `h-[130px] w-[400px] p-[10px] rounded-2xl bg-gradient-to-l from-[#ff33ff] to-[#ffff33] relative cursor-pointer transition-all duration-300 hover:scale-155 hover:shadow-xl border-2 border-[#33ffff]`,
+    card4: `h-[130px] w-[400px] p-[10px] rounded-2xl bg-gradient-to-l from-[#ff33ff] to-[#ffff33] relative cursor-pointer transition-all duration-300 hover:scale-155 hover:shadow-xl border-2 border-[#33ffff]`,
+    card4: `h-[130px] w-[400px] p-[10px] rounded-2xl bg-gradient-to-l from-[#ff33ff] to-[#ffff33] relative cursor-pointer transition-all duration-300 hover:scale-155 hover:shadow-xl border-2 border-[#33ffff]`,
     cardCross: `h-[180px] w-[125px] rounded-3xl absolute bottom-[20px] left-[20px] transition-all duration-300 hover:scale-105 flex  overflow-hidden`,
   }
   return (
