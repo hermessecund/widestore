59c003d8b2fb9e20839198041c0309760be8463d
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-12 21:52:37 +0000

    Update AmazonCoin.sol

--- smart-contract/contracts/AmazonCoin.sol
+++ smart-contract/contracts/AmazonCoin.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract AmazonCoin is ERC20, Ownable {
-    constructor() ERC20("Amazon Coin", "AC") {}
+    constructor() ERC20("hermessecund", "HS") {}
 
     function mint(uint256 amount) public payable {
         require(msg.value == amount * 0.0001 ether, "invalid amount of ether");
