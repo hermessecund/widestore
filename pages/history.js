891c49140bad58915cc732dc4cd7b9bd6342aa35
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-12 02:12:40 +0000

    Update history.js

--- pages/history.js
+++ pages/history.js
@@ -7,9 +7,9 @@ import Transaction from '../components/Transaction'
 
 const history = () => {
   const styles = {
-    container: `h-full w-full flex bg-[#000] text-black`,
+    container: `h-full w-full flex bg-[#000] text-white`,
     main: `w-full h-full flex flex-col mt-[50px]`,
-    tableContainer: `w-full h-full flex flex-col p-[100px] justify-center`,
+    tableContainer: `w-full h-full flex flex-col p-[0px] justify-center`,
     pageTitle: `text-2xl font-bold text-left mt-[50px] mb-[30px]`,
     transactions: `flex gap-[50px] flex-row flex-wrap`,
   }
