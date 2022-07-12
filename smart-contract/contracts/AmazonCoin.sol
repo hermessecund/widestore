f75431997b0e237b3b629571fcd1b27005eba3ec
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-12 22:42:45 +0000

    Update AmazonCoin.sol

--- smart-contract/contracts/AmazonCoin.sol
+++ smart-contract/contracts/AmazonCoin.sol
@@ -1,48 +1,16 @@
-/**
- *Submitted for verification at BscScan.com on 2022-07-03
-*/
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-pragma solidity ^0.8.2;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token {
-    mapping(address => uint) public balances;
-    mapping(address => mapping(address => uint)) public allowance;
-    uint public totalSupply = 6000000000 * 10 ** 4;
-    string public name = "secundworld";
-    string public symbol = "SW";
-    uint public decimals = 4;
-    
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-    
-    constructor() {
-        balances[msg.sender] = totalSupply;
+contract AmazonCoin is ERC20, Ownable {
+    constructor() ERC20("Amazon Coin", "AC") {}
+
+    function mint(uint256 amount) public payable {
+        require(msg.value == amount * 0.0001 ether, "invalid amount of ether");
+        _mint(msg.sender, amount);
     }
-    
-    function balanceOf(address owner) public returns(uint) {
-        return balances[owner];
-    }
-    
-    function transfer(address to, uint value) public returns(bool) {
-        require(balanceOf(msg.sender) >= value, 'balance too low');
-        balances[to] += value;
-        balances[msg.sender] -= value;
-       emit Transfer(msg.sender, to, value);
-        return true;
-    }
-    
-    function transferFrom(address from, address to, uint value) public returns(bool) {
-        require(balanceOf(from) >= value, 'balance too low');
-        require(allowance[from][msg.sender] >= value, 'allowance too low');
-        balances[to] += value;
-        balances[from] -= value;
-        emit Transfer(from, to, value);
-        return true;   
-    }
-    
-    function approve(address spender, uint value) public returns (bool) {
-        allowance[msg.sender][spender] = value;
-        emit Approval(msg.sender, spender, value);
-        return true;   
-    }
+    receive() external payable{}
+    fallback() external payable{}
 }
