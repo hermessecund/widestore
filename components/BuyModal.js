ea34c372c569a66777d3e8ae424596e431596aa2
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-13 11:45:22 +0000

    Update BuyModal.js

--- components/BuyModal.js
+++ components/BuyModal.js
@@ -3,10 +3,11 @@ import { IoIosClose } from 'react-icons/io'
 import { AmazonContext } from '../context/AmazonContext'
 import { HashLoader } from 'react-spinners'
 import Link from 'next/link'
+import logo from '../assets/ico_logo.png'
 
 const BuyModal = ({ close, buyTokens }) => {
   const styles = {
-    container: `h-full w-full flex bg-[#000] text-white flex-col `,
+    container: `h-full w-full flex bg-[#000] text-white `,
     closeX: `w-full h-[50px] flex items-center justify-end mb-[10px]`,
     title: `text-3xl font-bold flex flex-1 items-center mt-[20px] justify-center mb-[40px]`,
     content: `flex w-full mb-[20px] text-xl justify-center`,
@@ -15,7 +16,7 @@ const BuyModal = ({ close, buyTokens }) => {
     price: `w-full h-full flex justify-center items-center mt-[20px] font-bold text-3xl`,
     buyBtn: `w-[20%] h-[30px] bg-[#33ffff] mt-[20px] rounded-lg p-[5px] flex mx-auto text-black justify-center items-center cursor-pointer`,
     loaderContainer: `w-full h-[400px] flex items-center justify-center`,
-    loader: `w-full h-full flex items-center justify-center`,
+    loader: `w-full h-full flex items-center justify-center logo `,
     etherscan: `w-full h-full flex items-center justify-center text-green-500 text-2xl mt-[20px] font-bold cursor-pointer`,
     success: `w-full h-full flex items-center justify-center text-xl mt-[20px] font-bolder`,
   }
